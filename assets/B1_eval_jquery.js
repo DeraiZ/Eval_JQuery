9597151e70a1b4f1df082e67d0fc3ae0a121ed47
DeraiZ <91881453+DeraiZ@users.noreply.github.com>
2022-01-12 09:54:07 +0100

    question 1

--- assets/B1_eval_jquery.js
+++ assets/B1_eval_jquery.js
@@ -1,7 +1,41 @@
-// /* --------------------------------------------------------------------------
-
 $(function () {
-    
-})
-
-// -------------------------------------------------------------------------- */
+            $("#toggleRedSquare").click(function () {
+                // Question1
+                $(".redsquare-app").toggleClass("is-active");
+                $('redsquare-controls').removeAttr('disabled')
+                $('.btn').toggleClass('btn-light')
+                $('.btn').toggleClass('btn-primary')
+                // Question 3
+                $()
+                $(document).keypress(function (e) {
+                    switch (e.whitch) {
+                        case 37:
+                            $(".square").finish().animate({
+                                left: "+=20px"
+                            })
+                            break;
+                        case 38:
+                            $(".square").finish().animate({
+                                top: "+=20px"
+                            })
+                            break;
+                        case 39:
+                            $(".square").finish().animate({
+                                left: "-=20px"
+                            })
+                            break;
+                        case 40:
+                            $(".square").finish().animate({
+                                top: "-=20px"
+                            })
+                            break;
+                    }
+                })
+            })
+            
+            
+            
+            
+            
+                
+            })
